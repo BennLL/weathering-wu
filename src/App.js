c3542607223f02cb6e70de6e37c0b6b5ae7a2985
BenLL <zihaoluo153@gmail.com>
2023-01-05 17:02:39 -0800

    Search Bar + locaiton API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { GEO_API_URL, geoAPIoptions } from "./components/geocityAPI";
+import "./App.css";
+
+// const APIrelated ={
+//   key:"49bdc31b415440304250deae9af0e13b",
+// }
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [City, setCity] = useState({});
+
+  const searchPressed = () => {
+    fetch(
+      "http://api.openweathermap.org/geo/1.0/direct?q=" +
+        search +
+        "&limit=5&appid=49bdc31b415440304250deae9af0e13b"
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setCity(result);
+      });
+
+    // fetch(`${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${search}`, geoAPIoptions)
+    //   .then((response) => response.json())
+    //   .then((response) => setCity(response))
+    //   .catch((err) => console.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Container">
+      <header className="Header">Weathering WU</header>
+      <div>
+        <input
+          type="text"
+          className="SearchBar"
+          placeholder="Enter a city"
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+
+        <button onClick={searchPressed}>Search</button>
+      </div>
+
+      <div>DropdownMenu placeholder</div>
+
+      <p>{City[0].name}</p>
+      <p>Longitude: {City[0].lon}</p>
+      <p>Latitude: {City[0].lat}</p>
+
+      <h2 className="Temp">Temperature Place holder</h2>
+
+      <h2 className="Condition">Condition Place holder</h2>
     </div>
   );
 }
 
 export default App;
+
+/*
+  TO DO:
+  - differnt cities with same name
+  - Styling
+  - Search bar
+  - implement lat lon and weather api
+*/
---
+++ src/components/geocityAPI.js
@@ -0,0 +1,9 @@
+export const geoAPIoptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '503d7f8e81mshea54325395994b2p188765jsn8d479a3f3221',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
