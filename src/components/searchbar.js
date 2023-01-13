59cb318ff49e0bd25365a2a61cea80144a585fcd
BenLL <zihaoluo153@gmail.com>
2023-01-12 16:19:46 -0800

    SearchBar works

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
+import { useState, useEffect } from "react";
 
-const Searchbar = () => {
+function Searchbar(props) {
   const [search, setSearch] = useState("");
-  const [City, setCity] = useState({});
+  const [result, setResult] = useState([]);
 
-  const searchPressed = async () => {
-    fetch(
-      "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        search +
-        "&limit=5&appid=49bdc31b415440304250deae9af0e13b"
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        setCity(result);
-      });
-  };
+  useEffect(() => {
+    if (search.length > 0) {
+      fetch(
+        "http://api.openweathermap.org/geo/1.0/direct?q=" +
+          search +
+          "&limit=5&appid=49bdc31b415440304250deae9af0e13b"
+      )
+        .then((res) => res.json())
+        .then((resData) => {
+          setResult([]);
+          console.log(resData);
+          for (const key in resData) {
+            let city = resData[key].name;
+            if (city.slice(0, search.length).indexOf(search !== -1)) {
+              setResult((prevResult) => {
+                return [...prevResult, resData[key]];
+              });
+            }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      setResult([]);
+    }
+  }, [search]);
 
   return (
     <div className="Container">
@@ -26,18 +42,20 @@ const Searchbar = () => {
           className="SearchBar"
           placeholder="Enter a city"
           onChange={(e) => setSearch(e.target.value)}
+          value={search}
         ></input>
-
-        <button onClick={searchPressed}>Search</button>
       </div>
-      <h2>{}</h2>
-      <div>DropdownMenu placeholder</div>
-
-      <h2 className="Temp">Temperature Place holder</h2>
-
-      <h2 className="Condition">Condition Place holder</h2>
+      <div>
+        {result.map((result, index) => (
+          <div className={`${result.lon} ${result.lat}`}>
+            <button className="SearchResult" >
+              {result.name + ", " + result.country + ", " + result.state}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default Searchbar;
