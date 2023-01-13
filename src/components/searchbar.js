f935bc354ed44e21741cd3e93c78c3f566822d0c
BenLL <zihaoluo153@gmail.com>
2023-01-12 16:38:52 -0800

    timeout API call

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,33 +4,40 @@ import { useState, useEffect } from "react";
 function Searchbar(props) {
   const [search, setSearch] = useState("");
   const [result, setResult] = useState([]);
+  const [city, setCity] = useState([]);
 
   useEffect(() => {
-    if (search.length > 0) {
-      fetch(
-        "http://api.openweathermap.org/geo/1.0/direct?q=" +
-          search +
-          "&limit=5&appid=49bdc31b415440304250deae9af0e13b"
-      )
-        .then((res) => res.json())
-        .then((resData) => {
-          setResult([]);
-          console.log(resData);
-          for (const key in resData) {
-            let city = resData[key].name;
-            if (city.slice(0, search.length).indexOf(search !== -1)) {
-              setResult((prevResult) => {
-                return [...prevResult, resData[key]];
-              });
+    let timer = setTimeout(() => {
+      if (search.length > 0) {
+        fetch(
+          "http://api.openweathermap.org/geo/1.0/direct?q=" +
+            search +
+            "&limit=5&appid=49bdc31b415440304250deae9af0e13b"
+        )
+          .then((res) => res.json())
+          .then((resData) => {
+            setResult([]);
+            console.log(resData);
+            for (const key in resData) {
+              let city = resData[key].name;
+              if (city.slice(0, search.length).indexOf(search !== -1)) {
+                setResult((prevResult) => {
+                  return [...prevResult, resData[key]];
+                });
+              }
             }
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      setResult([]);
-    }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        setResult([]);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [search]);
 
   return (
@@ -48,9 +55,12 @@ function Searchbar(props) {
       <div>
         {result.map((result, index) => (
           <div className={`${result.lon} ${result.lat}`}>
-            <button className="SearchResult" >
+            <button className="SearchResult">
               {result.name + ", " + result.country + ", " + result.state}
             </button>
+            <div>
+                <p className = "lonlat">Latitude: {result.lat} Longitude: {result.lon}</p>
+            </div>
           </div>
         ))}
       </div>
